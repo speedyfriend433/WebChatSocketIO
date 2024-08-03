d877d99ec61ff416b2f308a9dc56f57be91ba499
speedyfriend433 <speedyfriend13@gmail.com>
2024-08-03 23:41:38 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
